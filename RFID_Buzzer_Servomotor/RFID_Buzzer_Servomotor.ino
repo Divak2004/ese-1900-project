48ea47c455c5a2162108215fdecc48add46f4903
David Fu <fu_david@outlook.com>
2025-04-15 18:09:10 -0400

    Adjust sensor code

--- RFID_Buzzer_Servomotor/RFID_Buzzer_Servomotor.ino
+++ RFID_Buzzer_Servomotor/RFID_Buzzer_Servomotor.ino
@@ -6,7 +6,7 @@
  * Created by FILIPEFLOP
  * 
  */
- 
+
 // #include <SPI.h>
 #include <Servo.h>
 int sensorState = 0;
@@ -14,7 +14,7 @@ int sensorState = 0;
 #define trigPin 13
 #define echoPin 12
 #define pirPin 7
- 
+
 // #define SS_PIN 10
 // #define RST_PIN 9
 Servo myservo;
@@ -25,24 +25,23 @@ Servo myservo;
 
 // int buzzpin = /* SET THIS PIN*/
 
-int pos = 0;    // variable to store the servo position
+int pos = 0;  // variable to store the servo position
 
-void setup() 
-{
-  Serial.begin(9600);   // Initiate a serial communication
+void setup() {
+  Serial.begin(9600);  // Initiate a serial communication
   // SPI.begin();      // Initiate  SPI bus
-  myservo.attach(8, 544, 2400); // attaches the servo on pin 8 to the servo and latter two arguments will give us a full 180-degree servo range
+  myservo.attach(9);  // attaches the servo on pin 9 to the servo and latter two arguments will give us a full 180-degree servo range
 
   // define pin modes
-  pinMode (pirPin, INPUT);
-  pinMode (trigPin, OUTPUT);
-  pinMode (echoPin, INPUT);
+  pinMode(pirPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 
   Serial.println("Start");
   Serial.println();
+  myservo.writeMicroseconds(1480);  
 }
-void loop() 
-{
+void loop() {
   long duration , distance;
   digitalWrite (trigPin , LOW ); // start trig at 0
   delayMicroseconds (2);
@@ -57,32 +56,60 @@ void loop()
   Serial.print("distance:");
   Serial.println(distance);
 
-  sensorState = digitalRead(pirPin);
-  Serial.println(sensorState);
+  // sensorState = digitalRead(pirPin);
+  // Serial.println(sensorState);
+
+
+  // //delay(5000);
+  // myservo.writeMicroseconds(1425);  
+  // delay(2000); // rotate for 2 seconds
+  //   // Stop
+  // myservo.writeMicroseconds(1480);  
+  // delay(2000); // stay still for 2 seconds
   
-  if (sensorState == HIGH) { 
-    // Motion detected
-    Serial.println("Motion Detected!");
-    pos= 135;
-    myservo.write(pos); // Move the servo to its new position (either 95 or 0)
-    delay(2000);
-    pos= 0;
-    myservo.write(pos); // Move the servo to its new position (either 95 or 0)
-    delay(1000);
-  } else {
-    Serial.println("No motion detected");
-    delay(1000);
-  }
+  // // Rotate counterclockwise
+  // myservo.writeMicroseconds(1530);  
+  // delay(2000); // rotate for 2 seconds
+  
+  // // Stop again
+  // myservo.writeMicroseconds(1480);  
+  // delay(2000);
+    // pos= 90;
+    // myservo.write(pos); // Move the servo to its new position (either 95 or 0)
+    // Serial.println();
+    // delay(1000);
+    // pos= 96;
+    // myservo.write(pos); // Move the servo to its new position (either 95 or 0)
+    // delay(1000);
+  
 
   // Change this so that if the IR detects something we turn the servo motor
-  // if (distance < 10)
-  // {
-  //   pos= 95;
-  //   myservo.write(pos); // Move the servo to its new position (either 95 or 0)
-  //   Serial.println();
-  //   delay(1000);
-  //   pos= 0;
-  //   myservo.write(pos); // Move the servo to its new position (either 95 or 0)
-  //   delay(1000);
-  // }
-} 
+  if (distance < 10)
+  {
+    // for (int i = 0; i < 90; i++) {
+    //   myservo.write(i);
+    //   delay(10);
+    // }
+    // for (int i = 90; i >= 0; i++) {
+    //   myservo.write(i);
+    //   delay(10);
+    // }
+    myservo.writeMicroseconds(1435);  
+    delay(2000); // rotate for 2 seconds
+      // Stop
+    Serial.println("Rotate clockwise");
+    myservo.writeMicroseconds(1480);  
+    delay(2000); // stay still for 2 seconds
+  
+    
+    // Rotate counterclockwise
+    myservo.writeMicroseconds(1530);  
+    delay(2000); // rotate for 2 seconds
+    Serial.println("Rotate counterclockwise");
+    // Stop again
+    myservo.writeMicroseconds(1480);  
+    delay(2000);
+  }
+
+  delay(2000);
+}
